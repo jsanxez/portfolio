6dd0560fc58aa40be39c07b0fa641abd14bb0c12
Jose Sanchez <jsanxez7@gmail.com>
2024-04-10 12:22:24 -0500

    feat (slider): wip. handle one image at the time and change to the next, close slider

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,48 +4,67 @@ export default function Image ({ imageSet }) {
   const [showModal, setShowModal] = useState(false)
 
   const handleClick = () => { setShowModal(true) }
+  const handleSlider = () => {
+    setShowModal(false)
+  }
 
   return (
     <>
       <div>
         <img src={imageSet[0].path} alt={imageSet[0].alt} onClick={handleClick} className='image w-80 rounded-2xl aspect-video object-cover border border-purple-700' />
         {imageSet.length > 1 && <div class='w-1 h-1 rounded-full bg-purple-700 mx-auto mt-2' />}
-        {showModal && <Lightbox imageSet={imageSet} />}
+        {showModal && <Lightbox imageSet={imageSet} onClose={handleSlider} />}
       </div>
     </>
   )
 }
 
-function Lightbox ({ imageSet }) {
+function Lightbox ({ imageSet, onClose }) {
+  imageSet = imageSet.map(img => ({ ...img, isVisible: false }))
+  const modImages = imageSet.map(img => ({ ...img }))
+  modImages[0].isVisible = true
+  const [images, setImages] = useState(modImages)
   const [index, setIndex] = useState(0)
-  const iterateImage = () => {
-    if (index < imageSet.length - 1) { setIndex(index + 1) } else { setIndex(0) }
-  }
 
-  console.log(imageSet)
+  const handleNext = (e) => {
+    e.stopPropagation()
+    if (index < imageSet.length - 1) {
+      const i = index + 1
+      const updatedImages = imageSet.map(img => ({ ...img }))
+      updatedImages[i].isVisible = true
+      setIndex(i)
+      setImages(updatedImages)
+    }
+  }
 
   return (
     <>
-      <div className='fixed inset-0 flex gap-4 items-center justify-center bg-black/80'>
+      <div onClick={() => { onClose() }} className='fixed inset-0 flex gap-4 items-center justify-center bg-black/80'>
         <Arrow direction='previous' />
         <div className='w-[1000px] border-4 border-yellow-300 overflow-hidden'>
           <p className='text-center text-white mb-4'>{`${index + 1} / ${imageSet.length}`}</p>
-          {imageSet.map(img => (
-            <div key={img.path}>
-              <img src={img.path} className='w-full aspect-video object-contain' />
-              <p className='text-white text-center mt-4'>{img.alt}</p>
-            </div>
+          {images.map(img => (
+            <Slide key={img.path} path={img.path} altText={img.alt} isVisible={img.isVisible} />
           ))}
         </div>
-        <Arrow direction='next' />
+        <Arrow direction='next' changeImage={handleNext} />
       </div>
     </>
   )
 }
 
-function Arrow ({ direction }) {
+function Slide ({ path, altText, isVisible }) {
   return (
-    <button className='inline-flex justify-center items-center rounded-full bg-white w-10 h-10'>
+    <div key={path} className={!isVisible && 'hidden'}>
+      <img src={path} className='w-full aspect-video object-contain' />
+      <p className='text-white text-center mt-4'>{altText}</p>
+    </div>
+  )
+}
+
+function Arrow ({ direction, changeImage }) {
+  return (
+    <button onClick={changeImage} className='inline-flex justify-center items-center rounded-full bg-white w-10 h-10'>
       {direction === 'next'
         ? <svg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'><path d='M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z' /></svg>
         : <svg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'><path d='M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z' /></svg>}
