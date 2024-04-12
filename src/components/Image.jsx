102a482affa5b21b638ac273ccd5a1a1f3e0a5d4
Jose Sanchez <jsanxez7@gmail.com>
2024-04-11 22:12:12 -0500

    feat, style (slider): update image container to fit almost all the screen

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,9 +4,7 @@ export default function Image ({ imageSet }) {
   const [showModal, setShowModal] = useState(false)
 
   const handleClick = () => { setShowModal(true) }
-  const handleSlider = () => {
-    setShowModal(false)
-  }
+  const handleSlider = () => { setShowModal(false) }
 
   return (
     <>
@@ -60,15 +58,17 @@ function Lightbox ({ imageSet, onClose }) {
 
   return (
     <>
-      <div onClick={() => { onClose() }} className='fixed inset-0 flex gap-4 items-center justify-center bg-black/60 backdrop-blur-xl'>
-        <Arrow direction='previous' changeImage={handlePrevious} />
-        <div className='w-[1000px]'>
-          <p className='text-center text-white mb-4'>{`${index + 1} / ${imageSet.length}`}</p>
-          {images.map(img => (
-            <Slide key={img.path} path={img.path} altText={img.alt} isVisible={img.isVisible} />
-          ))}
+      <div onClick={() => { onClose() }} className='fixed inset-0 flex justify-center items-center bg-black/60 backdrop-blur-xl'>
+        <div className='flex items-center  justify-center gap-4 mx-4 w-[1000px] h-[90vh]'>
+          <Arrow direction='previous' changeImage={handlePrevious} />
+          <div className='self-stretch grow flex flex-col justify-center gap-4'>
+            <p className='text-center text-white'>{`${index + 1} / ${imageSet.length}`}</p>
+            {images.map(img => (
+              <Slide key={img.path} path={img.path} altText={img.alt} isVisible={img.isVisible} />
+            ))}
+          </div>
+          <Arrow direction='next' changeImage={handleNext} />
         </div>
-        <Arrow direction='next' changeImage={handleNext} />
       </div>
     </>
   )
@@ -79,8 +79,10 @@ function Slide ({ path, altText, isVisible }) {
     e.stopPropagation()
   }
   return (
-    <div key={path} className={`${!isVisible && 'hidden'} aspect-video`}>
-      <img src={path} onClick={handleClick} className='h-full mx-auto' />
+    <div className={`${!isVisible && 'hidden'} max-h-[calc(100%-40px)]`}>
+      <div className='h-[calc(100%-40px)]'>
+        <img src={path} alt={altText} onClick={handleClick} className='h-full mx-auto object-contain' />
+      </div>
       <p className='text-white text-center mt-4'>{altText}</p>
     </div>
   )
@@ -88,7 +90,7 @@ function Slide ({ path, altText, isVisible }) {
 
 function Arrow ({ direction, changeImage }) {
   return (
-    <button onClick={changeImage} className='inline-flex justify-center items-center rounded-full bg-white w-10 h-10'>
+    <button onClick={changeImage} className='shrink-0 inline-flex justify-center items-center rounded-full bg-white w-10 h-10'>
       {direction === 'next'
         ? <svg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'><path d='M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z' /></svg>
         : <svg xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 -960 960 960' width='24'><path d='M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z' /></svg>}
