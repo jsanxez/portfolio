dcfba60c9fae7f8c5d9010054912ddf1138549b3
Jose Sanchez <jsanxez7@gmail.com>
2024-11-12 01:18:22 -0500

    chore: linting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,36 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content'
 
 const notesCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      author: z.string(),
-      color: z.array(z.string()),
-      textColor: z.string(),
-      featured: z.boolean().optional()
-    })
-});
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    color: z.array(z.string()),
+    textColor: z.string(),
+    featured: z.boolean().optional()
+  })
+})
 
 const projectsCollection = defineCollection({
-    type: 'content',
-    schema: ({ image }) => z.object({
-      title: z.string(),
-      description: z.string(),
-      date: z.date(),
-      index: z.number().int(),
-      status: z.string().optional(),
-      image: z.array(z.object({
-        path: image(),
-        alt: z.string()
-      })),
-      links: z.object({
-        code: z.string().optional(),
-        site: z.string().optional(),
-      }).optional()
-    })
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    index: z.number().int(),
+    status: z.string().optional(),
+    image: z.array(z.object({
+      path: image(),
+      alt: z.string()
+    })),
+    links: z.object({
+      code: z.string().optional(),
+      site: z.string().optional()
+    }).optional()
+  })
 })
 
 export const collections = {
   notes: notesCollection,
-  projects: projectsCollection,
-};
+  projects: projectsCollection
+}
